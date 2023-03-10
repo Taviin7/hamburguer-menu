a116c52e04267e7bfbf5959fea2730b3bad15d3a
Taviin7 <otaviorl711@gmail.com>
2023-03-10 17:40:07 -0300

    menu-hamburguer

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import { Contato } from "./src/Pages/Contato";
+import { Inicio } from "./src/Pages/Inicio";
+import { Notificacoes, Notification } from "./src/Pages/Notificacoes";
+
+const Menu = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Menu.Navigator>
+        <Menu.Screen
+          name="Inicio"
+          options={{
+            headerTitle: "Inicio",
+          }}
+          component={Inicio}
+        />
+        <Menu.Screen name="Contato" component={Contato} />
+        <Menu.Screen name="Notificacões" component={Notificacoes} />
+      </Menu.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
